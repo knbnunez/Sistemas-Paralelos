73f6092d0e4b9892c6d1bc9fac5581755c2b5f84
knbnunez <knbnunez@gmail.com>
2024-04-30 10:41:43 -0300

    Se corrigió asignaciones de variables mal situadas. Se sube el archivo de dependencias requirements.txt

--- Tp-MT-1/image_par.py
+++ Tp-MT-1/image_par.py
@@ -7,6 +7,8 @@ from numba import jit, prange
 
 @jit(parallel=True)
 def convert_to_grayscale(image, gray):
+    H, W, _ = image.shape # Obtener el ancho y alto de la imagen
+    gray = np.zeros((H, W), dtype=np.uint8) # Inicializar la matriz de la imagen en escala de grises
     for x in prange(H):
         for y in prange(W):
             r, g, b = image[x][y]
@@ -15,32 +17,25 @@ def convert_to_grayscale(image, gray):
     
 
 if __name__ == '__main__':
-    # No me fue posible hacerlo funcionar correctamente en un Foreach, no lograba terminar de calcular completo la imagen de 2000x1000
-    # IMAGE_PATHS = ['image_500x500.jpg', 'image_2000x1000.jpg']
-    
     # Comentar y descomentar la línea correspondiente
-    # IMAGE_PATHS = ['image_500x500.jpg']
-    IMAGE_PATHS = ['image_2000x1000.jpg']
+    # image_path = 'image_500x500.jpg'
+    image_path = 'image_2000x1000.jpg'
+    
+    image = Image.open(image_path) # Leer y almacenar la imagen
+    
+    init = time()
+    image = np.array(image) # Convertir imagen en matriz (arreglo de arreglos)
+    gray = convert_to_grayscale(image)
+    end = time() - init
 
-    for ip in IMAGE_PATHS:
-        init = time()
-        
-        image = Image.open(ip) # Leer y almacenar la imagen
-        image = np.array(image) # Convertirlo a array
-        H, W, _ = image.shape # Obtener el ancho y alto de la imagen
-        gray = np.zeros((H, W), dtype=np.uint8) # Inicializar la matriz de la imagen en escala de grises
-        gray = convert_to_grayscale(image, gray)
+    print(f"Para {image_path}, tiempo:", end)
 
-        end = time() - init
+    # Imagen original
+    plt.imshow(image)
+    plt.title('Imagen Original')
+    plt.show()
 
-        print(f"Para {ip}, tiempo:", end)
-
-        # Imagen original
-        plt.imshow(image)
-        plt.title('Imagen Original')
-        plt.show()
-
-        # Imagen en escala de grises
-        plt.imshow(gray, cmap='gray')
-        plt.title('Imagen en escala de grises')
-        plt.show()
+    # Imagen en escala de grises
+    plt.imshow(gray)
+    plt.title('Imagen en escala de grises')
+    plt.show()
